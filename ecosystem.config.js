3a14356e770a7d6e4e97985f58ce353eef5a815c
Mee(TB) <phongsak.ysd@gmail.com>
2025-08-20 16:38:04 +0700

    s4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       repo : 'git@github.com:phongsakk/ebl-test.git',
       path : '/home/ec2-user',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && npx pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     },
     server2 : {
